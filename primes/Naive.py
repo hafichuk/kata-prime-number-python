d38b4adf9f369e495fb90180e58b5c2a7b064225
Brad Hafichuk <brad@hafichuk.net>
2013-12-31 11:40:39 -0700

    Add 2

--- primes/Naive.py
+++ primes/Naive.py
@@ -1,2 +1,11 @@
 def naive(n):
+    """A prime number is a number, larger than 1, that can only be divided evenly by itself and 1"""
+    if (n < 2):
+        return []
+
+    primes = [2]
+
+    if (n in primes):
+        return [n]
+
     return []
