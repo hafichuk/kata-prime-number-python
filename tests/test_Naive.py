9f7832fe81eff9c473322d74f1a73f4a94ab25ad
Brad Hafichuk <brad@hafichuk.net>
2014-01-03 14:16:52 -0700

    Refactor tests

--- tests/test_Naive.py
+++ tests/test_Naive.py
@@ -5,57 +5,21 @@ class TestNaive(unittest.TestCase):
     def setUp(self):
         pass
 
-    def test_0(self):
-        n = 0
-        primes = []
-        self.assertEquals(primes, naive(n))
+    def test_primes(self):
+        numbers = [0, []],    \
+                  [1, []],    \
+                  [2, [2]],   \
+                  [3, [3]],   \
+                  [4, [2,2]], \
+                  [5, [5]],   \
+                  [6, [2,3]], \
+                  [7, [7]],   \
+                  [8, [2]*3], \
+                  [9, [3,3]], \
+                  [10, [2,5]],\
 
-    def test_1(self):
-        n = 1
-        primes = []
-        self.assertEquals(primes, naive(n))
+        for number in numbers:
+            self.assertEquals(number[1], naive(number[0]))
 
-    def test_2(self):
-        n = 2
-        primes = [2]
-        self.assertEquals(primes, naive(n))
 
-    def test_3(self):
-        n = 3
-        primes = [3]
-        self.assertEquals(primes, naive(n))
 
-    def test_4(self):
-        n = 4
-        primes = [2,2]
-        self.assertEquals(primes, naive(n))
-
-    def test_5(self):
-        n = 5
-        primes = [5]
-        self.assertEquals(primes, naive(n))
-
-    def test_6(self):
-        n = 6
-        primes = [2,3]
-        self.assertEquals(primes, naive(n))
-
-    def test_7(self):
-        n = 7
-        primes = [7]
-        self.assertEquals(primes, naive(n))
-
-    def test_8(self):
-        n = 8
-        primes = [2,2,2]
-        self.assertEquals(primes, naive(n))
-
-    def test_9(self):
-        n = 9
-        primes = [3,3]
-        self.assertEquals(primes, naive(n))
-
-    def test_10(self):
-        n = 10
-        primes = [2,5]
-        self.assertEquals(primes, naive(n))
